7c6c86ea56ef1838c0a98f41d735e6c9011e1e78
Thiago Varallo <thiagovaralllo@gmail.com>
2023-12-25 11:10:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
