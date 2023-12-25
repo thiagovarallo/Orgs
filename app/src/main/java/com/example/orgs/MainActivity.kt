1d37f80f6308dbdeb8ec2fc8a7a0a835bf64cc8b
Thiago Varallo <thiagovaralllo@gmail.com>
2023-12-25 11:25:58 -0300

    Buscando as views do layout na Activity

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -12,5 +12,15 @@ class MainActivity : Activity() {
 
         setContentView(R.layout.activity_main);
 
+        val title = findViewById<TextView>(R.id.title);
+        val descritions = findViewById<TextView>(R.id.description);
+        val price = findViewById<TextView>(R.id.price);
+
+
+        title.text = "Cesta de fruta";
+        descritions.text = "Uva, laranja, maça";
+        price.text = "19.99"
+
+        
     }
 }
